282fe467b453a5252dcc3e2ad6fef544da5bf953
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2021-02-19 20:31:18 +0100

    first draft of function for summation check

---
+++ R/checkSums.R
@@ -0,0 +1,20 @@
+checkSums <- function(x, cfg) {
+  sgroup <- extractVariableGroups(levels(x$variable),keepOrigNames = TRUE)
+  sgroup <- sgroup[grep(" \\(.*.\\)$",names(sgroup))]
+  failed <- NULL
+
+  for (i in 1:length(sgroup)) {
+    if (any(abs(filter(x,variable%in%sgroup[[i]]) %>%
+            group_by(region,period) %>%
+            summarise(grsum=sum(value),.groups = "drop") %>%
+            select("grsum")-
+            filter(arrange(x,region,period),variable==names(sgroup[i]))[["value"]])
+
+            >
+
+            0.00001 )
+        ) failed <- c(failed,names(sgroup[i]))
+  }
+  return(list(message="%# sums do not add up",
+              failed=failed))
+}
