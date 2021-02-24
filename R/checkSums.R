9c8f0cad2f4552f4844eb73d3567836a8cf35268
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2021-02-24 17:26:40 +0100

    expanded to check also for missing summation groups

--- R/checkSums.R
+++ R/checkSums.R
@@ -1,21 +1,29 @@
-checkSums <- function(x, cfg) {
+#' @importFrom mip extractVariableGroups
+#' @importFrom dplyr %>% filter group_by summarise select arrange
+
+checkSums <- function(x) {
   sgroup <- extractVariableGroups(levels(x$variable),keepOrigNames = TRUE)
-  sgroup <- sgroup[names(sgroup) %in% levels(x$variable)]
+  sgroup <- sgroup[names(sgroup) %in% unique(x$variable)]
   failed <- NULL
 
-  for (i in 1:length(sgroup)) {
-    if (any(abs(filter(x,variable%in%sgroup[[i]]) %>%
-            group_by(region,period) %>%
-            summarise(grsum=sum(value),.groups = "drop") %>%
-            select("grsum")-
-            filter(arrange(x,region,period),variable==names(sgroup[i]))[["value"]])
+  if (length(sgroup) > 0) for (i in 1:length(sgroup)) {
+    if (!any(sgroup[[i]]%in%unique(x$variable))) {
+      failed <- c(failed,names(sgroup[i]))
+    } else {
+      if (any(abs(filter(x,variable%in%sgroup[[i]]) %>%
+                  group_by(region,period) %>%
+                  summarise(grsum=sum(value),.groups = "drop") %>%
+                  select("grsum")-
+                  filter(arrange(x,region,period),variable==names(sgroup[i]))[["value"]])
 
-            >
+              >
 
-            0.00001 )
+              0.00001 )
 
-        ) failed <- c(failed,names(sgroup[i]))
+      ) failed <- c(failed,names(sgroup[i]))
+
+      }
   }
-  return(list(message="%# sums do not add up",
+  return(list(message="%# sums do not add up or are missing in the data or the cfg (shown here are the group totals)",
               failed=failed))
 }
